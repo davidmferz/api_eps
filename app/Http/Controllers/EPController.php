2470f692605d5da7a8b1a57a4baaa10bfba642ad
Oscar Ernesto Socrates Sanchez Villavicencio <oscar.sanchez@gruposellcom.com>
2019-02-06 12:55:18 -0600

    Se modifica config de SESSION

--- app/Http/Controllers/EPController.php
+++ app/Http/Controllers/EPController.php
@@ -140,7 +140,7 @@ class EPController extends Controller
                     'entrenadores' => $_SESSION['entrenadores'],
                     'perfil_ep' => $_SESSION['perfil_ep'],
                     'calificacion' => $_SESSION['calificacion'],
-                    'version' => Objeto::obtenerObjeto(953)['descripcion'],
+                    'version' => '3.0.3', //Objeto::obtenerObjeto(953)['descripcion'],
                     'clubs' => $_SESSION['clubs']
                 );
         return response()->json($out, 200);
