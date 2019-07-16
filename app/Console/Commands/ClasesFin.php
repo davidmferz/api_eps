2e23ee1faf320f45cd9563f89d2f2f2108f39b82
luis cosio <luis.cosio@sportsworld.com.mx>
2019-07-16 10:12:27 -0500

    prueba mail

--- app/Console/Commands/ClasesFin.php
+++ app/Console/Commands/ClasesFin.php
@@ -61,6 +61,7 @@ class ClasesFin extends Command
                 $value->host  = env('APP_URL');
                 $url          = $value->host . '/app-eps/#/calificacion/' . $value->idEventoInscripcion . '/' . $value->token;
                 Log::debug('Prueba mail');
+                Log::debug($value->mail);
                 Log::debug($url);
 
                 Mail::to($value->mail)->send(new SendEncuestaEvaluacion($value));
