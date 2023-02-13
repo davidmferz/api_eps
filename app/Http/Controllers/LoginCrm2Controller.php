37811e1d6f24620e6310c3c809d4510ae0f4d2c1
cosio <luis01cosio@gmail.com>
2023-02-13 12:13:36 -0600

    puesto

--- app/Http/Controllers/LoginCrm2Controller.php
+++ app/Http/Controllers/LoginCrm2Controller.php
@@ -158,7 +158,7 @@ class LoginCrm2Controller extends ApiController
         if (in_array($idEmpleado, ['15430'])) {
             return 'root';
         }
-        if (in_array($idPuesto, [4, 39, 50, 131, 142, 151, 163])) {
+        if (in_array($idPuesto, [4, 39, 50, 131, 142, 151, 72, 163])) {
             return 'coordinador';
         }
         if (in_array($idPuesto, [62, 67, 69, 77, 81, 99, 95, 104, 141])) {
